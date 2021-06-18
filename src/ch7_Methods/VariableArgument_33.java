d0808af304c5d96a9e596b9a7eae64b032cb95cb
Praveen-Wakchaure <praveen18798@gmail.com>
2021-06-18 13:22:10 +0530

    commit

---
+++ src/ch7_Methods/VariableArgument_33.java
@@ -0,0 +1,31 @@
+package ch7_Methods;
+
+public class VariableArgument_33 {
+
+	
+	public static int sum (int ...arr)  //<-- Variable arguments 
+	{
+		
+	   int result = 0;
+	     	
+	   for(int a:arr)
+		{
+			result += a;		
+		}	
+	   return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("result= " +sum(1));
+		
+		System.out.println("result= " +sum(1,2,3));
+		
+		System.out.println("result= " +sum(1,2,3,4));
+		
+		System.out.println("result= " +sum(1,2,3,50));
+		
+		System.out.println("result= " +sum(1,2,3,44));
+	}
+
+}
