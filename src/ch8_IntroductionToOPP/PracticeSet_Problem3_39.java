1819010bf119972c4780f795c15d4aa609e49dd1
Praveen-Wakchaure <praveen18798@gmail.com>
2021-06-22 10:47:53 +0530

    commit

---
+++ src/ch8_IntroductionToOPP/PracticeSet_Problem3_39.java
@@ -0,0 +1,52 @@
+package ch8_IntroductionToOPP;
+
+
+
+class Square
+{
+	int side;
+	
+	public int setSide(int s) 
+	{
+		side=s;
+		
+		return side;
+	}
+	
+	public int side()
+	{
+		return side;
+	}
+	
+	public int area()
+	{
+		return side*side;
+	}
+	
+	public int perimeter()
+	{
+		return 4*side;
+	}
+}
+
+
+
+
+
+
+public class PracticeSet_Problem3_39 {
+	
+
+	public static void main(String[] args) {
+		
+		Square s = new Square();
+		
+		s.setSide(5);
+		
+		System.out.println("Side: "+s.side);
+		System.out.println("Area: "+s.area());
+		System.out.println("Perimeter: "+s.perimeter());
+		
+	}
+
+}
