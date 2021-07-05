a551e2ac1f241dd53cb4da73a9e4846256bfbbf7
Praveen-Wakchaure <praveen18798@gmail.com>
2021-07-05 12:27:18 +0530

    commit

---
+++ src/ch15_AdvancedJAVA_1/CollectionPackages_89.java
@@ -0,0 +1,24 @@
+package ch15_AdvancedJAVA_1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Stack;
+
+public class CollectionPackages_89 {
+
+	public static void main(String[] args) {
+		
+		//ArrayList<E>
+		
+		//Set<E>
+		
+		//TreeSet;
+		
+		//HashMap<K, V>
+		
+		//Stack<E>
+		
+	}
+
+}
---
+++ src/ch15_AdvancedJAVA_1/HashSet_95.java
@@ -0,0 +1,31 @@
+package ch15_AdvancedJAVA_1;
+
+import java.util.HashSet;
+
+public class HashSet_95 {
+
+	
+	public static void main(String[] args) {
+		
+		HashSet<Integer> hs = new HashSet<>(5);
+		
+		hs.add(501);
+		hs.add(502);
+		hs.add(503);
+		hs.add(504);
+		hs.add(505);
+		hs.add(506);
+		hs.add(507);
+		
+		hs.add(503);  //Cannot add Duplicate element (Collision)
+		
+		System.out.print("hs Array: ");
+		System.out.println(hs.clone());
+		
+		hs.clear();
+		
+		System.out.print("hs Array: ");
+		System.out.println(hs.clone());
+	}
+
+}
