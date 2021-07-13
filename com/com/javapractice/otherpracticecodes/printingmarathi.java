f8a251f6d2a8738cbc21f7d2a914e1ad86f4bb94
Praveen-Wakchaure <praveen18798@gmail.com>
2021-07-13 11:10:49 +0530

    commit

---
+++ com/com/javapractice/otherpracticecodes/printingmarathi.java
@@ -0,0 +1,11 @@
+package com.javapractice.otherpracticecodes;
+
+public class printingmarathi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("प्रवीण");
+		System.out.println("पुणे");
+	}
+
+}
